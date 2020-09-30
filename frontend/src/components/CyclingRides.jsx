e0eb4dd40f88199c71f62408946cb272068713b7
Hélène Baudrand <baudrand.helene@gmail.com>
2020-09-30 16:00:57 +0200

    Implemented static responsive draer

--- frontend/src/components/CyclingRides.jsx
+++ frontend/src/components/CyclingRides.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import Header from './Header';
+
 import MyProfile from './MyProfile';
+import ResponsiveDrawer from './ResponsiveDrawer';
 import RideForm from './RideForm';
 import RidesGallery from './RidesGallery';
 
 function CyclingRides() {
 	return (
 		<div className="cycling-rides">
-			<Header />
 			<RidesGallery />
 			<RideForm />
 			<MyProfile />
+			<ResponsiveDrawer />
 		</div>
 	);
 }
