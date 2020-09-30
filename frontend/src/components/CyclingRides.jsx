b7c6386b0d4e6be8161ae93b42316282fafa4cb1
Hélène Baudrand <baudrand.helene@gmail.com>
2020-09-30 16:17:24 +0200

    changed typography

--- frontend/src/components/CyclingRides.jsx
+++ frontend/src/components/CyclingRides.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 import MyProfile from './MyProfile';
 import ResponsiveDrawer from './ResponsiveDrawer';
 import RideForm from './RideForm';
 import RidesGallery from './RidesGallery';
 
+const THEME = createMuiTheme({
+	typography: {
+		fontFamily: `'Architects Daughter', cursive`,
+		fontSize: 16,
+	},
+});
+
 function CyclingRides() {
 	return (
 		<div className="cycling-rides">
-			<RidesGallery />
-			<RideForm />
-			<MyProfile />
-			<ResponsiveDrawer />
+			<MuiThemeProvider theme={THEME}>
+				<RidesGallery />
+				<RideForm />
+				<MyProfile />
+				<ResponsiveDrawer />
+			</MuiThemeProvider>
 		</div>
 	);
 }
