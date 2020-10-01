537cf20d55b4b96e87863c9fcd5ff95cabbd3802
Hélène Baudrand <baudrand.helene@gmail.com>
2020-10-01 15:28:32 +0200

    solved issues

--- frontend/src/components/NewsCarousel.jsx
+++ frontend/src/components/NewsCarousel.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import Carousel from 'react-material-ui-carousel';
 
-import { Paper, Button } from '@material-ui/core';
+import { Paper } from '@material-ui/core';
 
 import panoramic1 from '../assets/panoramic1.jpg';
 import panoramic2 from '../assets/panoramic2.jpg';
 import panoramic3 from '../assets/panoramic3.jpg';
 import panoramic4 from '../assets/panoramic4.jpg';
+//TODO check require vs import
+require('default-passive-events');
 
 function Project(props) {
 	return (
 		<Paper
-			className="Project"
 			style={{
 				backgroundImage: `url(${props.item.image})`,
 				backgroundRepeat: 'no-repeat',
@@ -23,8 +24,6 @@ function Project(props) {
 		>
 			<h2>{props.item.name}</h2>
 			<p>{props.item.description}</p>
-
-			<Button className="CheckButton">Check it out!</Button>
 		</Paper>
 	);
 }
@@ -36,7 +35,7 @@ const items = [
 		description: 'A PDF Reader specially designed for musicians.',
 	},
 	{
-		image: panoramic2,
+		image: panoramic4,
 		name: 'Hash Code 2019',
 		description:
 			'My Solution on the 2019 Hash Code by Google Slideshow problem.',
@@ -47,7 +46,7 @@ const items = [
 		description: 'A exciting mobile game game made in the Unity Engine.',
 	},
 	{
-		image: panoramic4,
+		image: panoramic2,
 		name: 'React Carousel',
 		description: 'A Generic carousel UI component for React using material ui.',
 	},
@@ -60,12 +59,7 @@ const animation = 'fade';
 function NewsCarousel() {
 	return (
 		<div>
-			<Carousel
-				animation={animation}
-				autoPlay={autoPlay}
-				timer={timer}
-				animation={animation}
-			>
+			<Carousel animation={animation} autoPlay={autoPlay} timer={timer}>
 				{items.map((item, index) => {
 					return <Project item={item} key={index} />;
 				})}
