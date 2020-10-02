6a84cc8705085e2a9d27851fddf66bcc36b4addb
Hélène Baudrand <baudrand.helene@gmail.com>
2020-10-02 16:48:58 +0200

    implemented dynamic gallery data

--- frontend/src/components/RidesGallery.jsx
+++ frontend/src/components/RidesGallery.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -11,87 +12,6 @@ import Typography from '@material-ui/core/Typography';
 import ventoux from '../assets/ventoux.jpg';
 import galibier from '../assets/galibier.jpg';
 import aspin from '../assets/aspin.jpg';
-
-const RIDES = [
-	{
-		id: 1,
-		name: 'Mont Ventoux',
-		picture: ventoux,
-		startLocation: 'Bédoin',
-		altitude: 1909,
-		mountain: 'Provence',
-		country: 'France',
-		kilometers: 22.7,
-		elevation: 1622,
-		averageSlope: 7.15,
-		maxSlope: 10.8,
-	},
-	{
-		id: 2,
-		name: 'Col du Galibier',
-		picture: galibier,
-		startLocation: 'Col du Lautaret',
-		altitude: 2642,
-		mountain: 'Arves et Grandes Rousses',
-		country: 'France',
-		kilometers: 8.5,
-		elevation: 585,
-		averageSlope: 6.88,
-		maxSlope: 12.1,
-	},
-	{
-		id: 3,
-		name: "Col d'Aspin",
-		picture: aspin,
-		startLocation: 'Arreau',
-		altitude: 1489,
-		mountain: 'Pyrénées centrales',
-		country: 'France',
-		kilometers: 12,
-		elevation: 779,
-		averageSlope: 6.49,
-		maxSlope: 9.5,
-	},
-	{
-		id: 4,
-		name: 'Mont Ventoux',
-		picture: ventoux,
-		startLocation: 'Bédoin',
-		altitude: 1909,
-		mountain: 'Provence',
-		country: 'France',
-		kilometers: 22.7,
-		elevation: 1622,
-		averageSlope: 7.15,
-		maxSlope: 10.8,
-	},
-	{
-		id: 5,
-		name: 'Col du Galibier',
-		picture: galibier,
-		startLocation: 'Col du Lautaret',
-		altitude: 2642,
-		mountain: 'Arves et Grandes Rousses',
-		country: 'France',
-		kilometers: 8.5,
-		elevation: 585,
-		averageSlope: 6.88,
-		maxSlope: 12.1,
-	},
-	{
-		id: 6,
-		name: "Col d'Aspin",
-		picture: aspin,
-		startLocation: 'Arreau',
-		altitude: 1489,
-		mountain: 'Pyrénées centrales',
-		country: 'France',
-		kilometers: 12,
-		elevation: 779,
-		averageSlope: 6.49,
-		maxSlope: 9.5,
-	},
-];
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -107,10 +27,23 @@ const useStyles = makeStyles((theme) => ({
 
 function RidesGallery() {
 	const classes = useStyles();
+	const [rides, setRides] = useState([]);
+	const [error, setError] = useState('');
+
+	useEffect(() => {
+		axios
+			.get('/api/rides')
+			.then((res) => {
+				setRides(res.data);
+			})
+			.catch((err) => {
+				setError(err.message);
+			});
+	}, []);
 
 	return (
 		<div className="rides-gallery">
-			{RIDES.map((ride) => (
+			{rides.map((ride) => (
 				<Card className={classes.root} key={ride.id}>
 					<CardHeader title={ride.name} subheader={ride.mountain} />
 					<CardMedia
