a412bb3d9646576ddd49b560c6a1636f53845803
Hélène Baudrand <baudrand.helene@gmail.com>
2020-10-08 09:16:59 +0200

    added restrictions to multer config

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,4 +1,6 @@
 const multer = require('multer');
+const maxSize = 2 * 1024 * 1024;
+const util = require('util');
 
 const MIME_TYPES = {
 	'image/jpg': 'jpg',
@@ -8,7 +10,7 @@ const MIME_TYPES = {
 
 const storage = multer.diskStorage({
 	destination: (req, file, callback) => {
-		callback(null, 'images');
+		callback(null, __basedir + 'images');
 	},
 	filename: (req, file, callback) => {
 		const name = file.originalname.split(' ').join('_');
@@ -17,4 +19,11 @@ const storage = multer.diskStorage({
 	},
 });
 
-module.exports = multer({ storage: storage }).single('image');
+const uploadPicture = multer({
+	storage: storage,
+	limits: { fileSize: maxSize },
+}).single('picture');
+
+const uploadPictureMiddlesware = util.promisify(uploadPicture);
+
+module.exports = uploadPictureMiddlesware;
